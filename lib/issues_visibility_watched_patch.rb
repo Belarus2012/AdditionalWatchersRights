8315db557f9324e794731c92ec713611c2b6f8c3
Dmitriy Brodnitskiy <dmitriy.brodnitskiy@twinslash.com>
2013-01-21 17:31:05 +0300

    add statement to issues visibility

--- lib/issues_visibility_watched_patch.rb
+++ lib/issues_visibility_watched_patch.rb
@@ -3,7 +3,7 @@ module IssuesVisibilityWatchedPatch
 	def self.included(base) 
 		base.class_eval do 
 
-		Role::ISSUES_VISIBILITY_OPTIONS << ['watcher', :label_issues_visibility_watched] 
+		Role::ISSUES_VISIBILITY_OPTIONS << ['watched', :label_issues_visibility_watched] 
 		end			
 	end	
 
